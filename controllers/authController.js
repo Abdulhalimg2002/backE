5e4d929e4425e6a8fd854b9dfed01a95eae07ab0
codeawy <abdulhalim2002ii@gmail.com>
2026-05-22 16:16:19 +0300

    make some change

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,15 +18,13 @@ const sendToken = (res, user) => {
     { expiresIn: "7d" }
   );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
-
-  res.setHeader("Authorization", `Bearer ${token}`); // 👈 ADD THIS
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none", // 🔥 أو none (إذا اشتغل مشاكل)
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
   return token;
 };
